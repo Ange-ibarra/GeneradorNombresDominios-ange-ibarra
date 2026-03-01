d1a9b01e78a9f68fa988bd784036a75c5544291b
Angelica Ibarra <angelicaavy@hotmail.es>
2026-03-01 21:48:57 +0000

    done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let pronoun = ['the', 'our', 'some'];
+  let adj = ['great', 'big', 'cool'];
+  let noun = ['jogger', 'racoon', 'blog'];
+
+  let domainNames = [];
+  
+  for(let p = 0; p < pronoun.length; p++) {
+    for(let a = 0; a < adj.length; a++) {
+      for(let n = 0; n < noun.length; n++) {
+        console.log(pronoun[p] + adj[a] + noun[n]+'.com'); 
+      }
+    }
+  }
+}
